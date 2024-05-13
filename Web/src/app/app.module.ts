6bce0333feaf56f8947e46b3f913f93bc483292b
FranklinMonro <fbg2468@gmail.com>
2024-05-13 09:32:59 +0000

    add components

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { RegisterComponent } from './register/register.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
