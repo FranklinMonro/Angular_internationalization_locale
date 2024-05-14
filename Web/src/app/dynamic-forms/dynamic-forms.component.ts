84c0e1826061a3dc19c029cec97d2b5d907159ab
FranklinMonro <fbg2468@gmail.com>
2024-05-14 13:40:34 +0000

    dynamic form register form components

---
+++ Web/src/app/dynamic-forms/dynamic-forms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamic-forms.component.html',
+  styleUrl: './dynamic-forms.component.scss'
+})
+export class DynamicFormsComponent {
+
+}
---
+++ Web/src/app/register-dynamic/register-dynamic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-dynamic',
+  templateUrl: './register-dynamic.component.html',
+  styleUrl: './register-dynamic.component.scss'
+})
+export class RegisterDynamicComponent {
+
+}
