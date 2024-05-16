5c5e4188fda61158ba0ebb722e84a0fb604189b5
FranklinMonro <fbg2468@gmail.com>
2024-05-16 12:08:30 +0000

    french lang

--- Web/src/app/register-localization/register-localization.component.ts
+++ Web/src/app/register-localization/register-localization.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-register-localization',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './register-localization.component.scss'
 })
 export class RegisterLocalizationComponent {
+  registerForm: FormGroup | undefined;
+  
+  selectedValue: string = DateTime.now().zoneName;
+  constructor(private formBuilder: FormBuilder,) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.registerForm = this.formBuilder.group({
+      firstname: [null, [Validators.required]],
+      surname: [null, [Validators.required]],
+      idNumber: [null, [Validators.required]],
+      luxon_date: [DateTime.now().toISO()!],
+      javascript_date: [new Date().toISOString()]
+    });
+  }
+
+  onSubmit() {
+    console.log('onSubmit');
+  }
 
 }
