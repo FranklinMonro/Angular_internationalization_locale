e9481d48aca88f80aa3093be06c4be65f9f3ca70
FranklinMonro <fbg2468@gmail.com>
2024-05-17 13:13:24 +0000

    service

--- Web/src/app/app.service.ts
+++ Web/src/app/app.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getRegisters = (): any => {
+    return this.httpClient.get<any>(`http://localhost:3000/api/register`, 
+    { observe: 'response' }).pipe(
+      map((response: HttpResponse<any>) => {
+        return response.body;
+      }),
+      catchError((error: HttpErrorResponse) =>  { throw new Error(error.message); }),
+    ).subscribe();
+  };
+
+  postRegisters = (): any => {
+    return this.httpClient.post<any>(`http://localhost:3000/api/register`, 
+    { observe: 'response' }).pipe(
+      map((response: HttpResponse<any>) => {
+        return response.body;
+      }),
+      catchError((error: HttpErrorResponse) =>  { throw new Error(error.message); }),
+    ).subscribe();
+  };
 }
